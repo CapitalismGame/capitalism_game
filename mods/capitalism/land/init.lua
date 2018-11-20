0d48dd6eae7ad667f097bd61ca6ecb5da1479333
rubenwardy <rw@rubenwardy.com>
2018-11-20 00:23:08 +0000

    Remove Zone class

--- mods/capitalism/land/init.lua
+++ mods/capitalism/land/init.lua
@@ -1,6 +1,5 @@
 land = {}
 
-dofile(minetest.get_modpath("land") .. "/zone.lua")
 dofile(minetest.get_modpath("land") .. "/api.lua")
 dofile(minetest.get_modpath("land") .. "/gui.lua")
 dofile(minetest.get_modpath("land") .. "/chatcmds.lua")
