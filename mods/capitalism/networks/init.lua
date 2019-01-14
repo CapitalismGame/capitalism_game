4daf6201e6b6392135e99ae69508b91fb229e238
rubenwardy <rw@rubenwardy.com>
2019-01-14 20:41:05 +0000

    Remove unused mods

--- mods/capitalism/networks/init.lua
+++
@@ -1,7 +0,0 @@
-print("[networks] loading...")
-
-dofile(minetest.get_modpath("networks") .. "/api.lua")
-networks.init()
-dofile(minetest.get_modpath("networks") .. "/communicators.lua")
-
-print("[networks] load")
